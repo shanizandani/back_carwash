d4ddd6f8c099e636f90a03d10473dc8e23311526
shanizandani <127876233+shanizandani@users.noreply.github.com>
2023-09-26 02:00:06 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fb10-5-28-176-19.ngrok-free.app','project_carwash.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fb10-5-28-176-19.ngrok-free.app','project-carwash.onrender.com']
 
 
 
