fc27dd75e9c6ac5b2a2618a75381fc725854faf0
shanizandani <127876233+shanizandani@users.noreply.github.com>
2023-09-28 00:53:47 +0300

    Update urls.py

--- base/api/urls.py
+++ base/api/urls.py
@@ -93,7 +93,7 @@ urlpatterns = [
         name="request-password-reset",
     ),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
